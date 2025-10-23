3174447b0662a5bf3fc8514284676a1512bb3d74
SandeshBorakar <sandeshborakar@gmail.com>
2025-10-24 04:06:31 +0530

    added comments

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let display = document.getElementById('display');
 
 function appendNumber(num) {
-    display.value += num;
+    display.value += num;  //appends the number in string format
 }
 
 function appendOperator(operator) {
-    display.value += operator;
+    display.value += operator; //appends the operator
 }
 
 function clearDisplay() {
-    display.value = '';
+    display.value = ''; // clears the screen for new calculation
 }
 
 function calculate() {
     try {
-        display.value = eval(display.value);
+        display.value = eval(display.value); // evaluates the given string and displays the results
     } catch (error) {
-        display.value = 'Error';
+        display.value = 'Error'; // diplays the errro has occured.
     }
 }
